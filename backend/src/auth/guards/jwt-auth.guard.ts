fd35bdf82fe7389fb9d07a5e4adbfbc352899a68
Quentin <quentinsolliez@outlook.fr>
2024-12-17 15:49:02 +0100

    feat: enhance JWT authentication with cookie support
    - Updated JwtAuthGuard to include cookie extraction for JWT tokens.
    - Modified JwtStrategy to streamline token extraction from cookies and authorization headers.
    - Improved security by ensuring JWT_SECRET is utilized for token validation.

--- backend/src/auth/guards/jwt-auth.guard.ts
+++ backend/src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ExtractJwt } from 'passport-jwt';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor() {
+    super({
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+        (request) => {
+          return request?.cookies?.token;
+        },
+      ]),
+    });
+  }
+}
--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
@@ -8,10 +7,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => {
-          return request?.cookies?.token;
-        },
         ExtractJwt.fromAuthHeaderAsBearerToken(),
+        (request) => request?.cookies?.token,
       ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
